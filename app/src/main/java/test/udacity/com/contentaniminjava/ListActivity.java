fe550b66c6fb8a88d7158fe8e711f5afd77420f9
Bernat Gomez <bernat.gomez@mobetia.com>
2017-07-18 17:19:09 +0200

    Initial commit

---
+++ app/src/main/java/test/udacity/com/contentaniminjava/ListActivity.java
@@ -0,0 +1,32 @@
+package test.udacity.com.contentaniminjava;
+
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.setContentView(R.layout.activity_list);
+
+        if (savedInstanceState == null) {
+            this.launchContentFragment();
+        }
+    }
+
+    private void launchContentFragment() {
+        FragmentManager mgr = this.getSupportFragmentManager();
+
+        FragmentTransaction trans = mgr.beginTransaction();
+
+        trans.replace(R.id.mainContainer, ListFragment.newInstance());
+
+        trans.commit();
+    }
+}
---
+++ app/src/main/java/test/udacity/com/contentaniminjava/ListFragment.java
@@ -0,0 +1,77 @@
+package test.udacity.com.contentaniminjava;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * Created by bernatgomez on 18/7/17.
+ */
+
+public class ListFragment extends Fragment {
+
+    @BindView(R.id.list)
+    protected RecyclerView list;
+
+
+    public static ListFragment newInstance() {
+        return new ListFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View v = inflater.inflate(R.layout.fragment_list, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.bindViews();
+    }
+
+    private void bindViews() {
+        ButterKnife.bind(this, this.getView());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+}
